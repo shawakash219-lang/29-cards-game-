8aa88f9d4b2d1a296379b0066fd934d27999ec95
shawakash219-lang <shawakash219@gmail.com>
2025-12-19 21:20:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCLsfetGGbvG5aeVHQSzVMXyzt395Buucg",
+  authDomain: "cards29-game.firebaseapp.com",
+  projectId: "cards29-game",
+  storageBucket: "cards29-game.firebasestorage.app",
+  messagingSenderId: "989522634372",
+  appId: "1:989522634372:web:7c9e6059119ccbd71e84d1"
+};
