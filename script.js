7106d51603099a04e21465b3b32a7473843348e9
shawakash219-lang <shawakash219@gmail.com>
2025-12-19 23:30:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
 alert("JS working");
+
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCLsfetGGbvG5aeVHQSzVMXyzt395Buucg",
   authDomain: "cards29-game.firebaseapp.com",
+  databaseURL: "https://cards29-game-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "cards29-game",
-  storageBucket: "cards29-game.firebasestorage.app",
+  storageBucket: "cards29-game.appspot.com",
   messagingSenderId: "989522634372",
   appId: "1:989522634372:web:7c9e6059119ccbd71e84d1"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
@@ -20,6 +26,7 @@ const playersDiv = document.getElementById("players");
 
 let playerName = "";
 let roomCode = "";
+let gameStarted = false;
 
 // Create Room
 function createRoom() {
@@ -54,7 +61,7 @@ function joinRoom() {
   enterGame();
 }
 
-// Enter Game
+// Enter Game (MAIN LOGIC)
 function enterGame() {
   menu.style.display = "none";
   game.style.display = "block";
@@ -62,8 +69,48 @@ function enterGame() {
 
   db.ref("rooms/" + roomCode + "/players").on("value", snap => {
     playersDiv.innerHTML = "";
+    let playerList = [];
+
     snap.forEach(p => {
       playersDiv.innerHTML += `<div>${p.key}</div>`;
+      playerList.push(p.key);
     });
+
+    // 4 players hone par cards baantna
+    if (playerList.length === 4 && !gameStarted) {
+      gameStarted = true;
+      distributeCards(playerList);
+    }
   });
+}
+
+// ---------------- CARD LOGIC ----------------
+
+// 29 game cards
+const suits = ["♠", "♥", "♦", "♣"];
+const values = ["7", "8", "9", "10", "J", "Q", "K", "A"];
+
+function createDeck() {
+  let deck = [];
+  for (let s of suits) {
+    for (let v of values) {
+      deck.push(v + s);
+    }
+  }
+  return deck;
+}
+
+function shuffle(deck) {
+  return deck.sort(() => Math.random() - 0.5);
+}
+
+function distributeCards(players) {
+  let deck = shuffle(createDeck());
+  let hands = {};
+
+  players.forEach(p => {
+    hands[p] = deck.splice(0, 8);
+  });
+
+  db.ref("rooms/" + roomCode + "/hands").set(hands);
 }
